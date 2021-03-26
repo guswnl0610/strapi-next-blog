4f5fddf208e05585b0a829ca1b312e93a91d4b91
hyunji Kim <mengkki@smartstudy.co.kr>
2021-03-26 19:26:08 +0900

    Add : singleArticle

--- backend/api/article/config/schema.graphql.js
+++ backend/api/article/config/schema.graphql.js
@@ -2,6 +2,7 @@ module.exports = {
   definition: `
   `,
   query: `
+    singleArticle(id: ID!, publicationState: PublicationState): Article!
     articlesByUser: [Article]!
   `,
   mutation: `
@@ -10,9 +11,31 @@ module.exports = {
   type: {},
   resolver: {
     Query: {
-      article: {
+      singleArticle: {
         description: "return a single article",
         policies: ["plugins::users-permissions.isAuthenticated"],
+        resolver: "application::article.article.findOne",
+        // resolverOf: "application::article.article.findOne",
+        // resolver: async (obj, options, ctx) => {
+        //   // console.log(ctx, obj, options);
+        //   // const result = await strapi.services.article.findOne({
+        //   //   id: options.id,
+        //   // });
+        //   return strapi.services.article.findOne({ id: options.id });
+        // },
+      },
+      articlesByUser: {
+        description: {
+          description: "return articles by loginned User",
+          resolverOf: "application::article.article.findOne",
+          resolver: async (obj, options, ctx) => {
+            const result = await strapi.services.article.findOne(options);
+
+            console.log(result);
+
+            return result;
+          },
+        },
       },
     },
     Mutation: {},
