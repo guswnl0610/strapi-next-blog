f77d7bfdf094ed014e0379b60999227b2604625d
hyunji Kim <mengkki@smartstudy.co.kr>
2021-03-29 14:25:35 +0900

    Add : article by user

--- backend/api/article/config/schema.graphql.js
+++ backend/api/article/config/schema.graphql.js
@@ -2,39 +2,19 @@ module.exports = {
   definition: `
   `,
   query: `
-    singleArticle(id: ID!, publicationState: PublicationState): Article!
     articlesByUser: [Article]!
   `,
   mutation: `
-    attatchArticleToUser(id: ID, userID: ID): Article!
   `,
   type: {},
   resolver: {
     Query: {
-      singleArticle: {
-        description: "return a single article",
-        policies: ["plugins::users-permissions.isAuthenticated"],
-        resolver: "application::article.article.findOne",
-        // resolverOf: "application::article.article.findOne",
-        // resolver: async (obj, options, ctx) => {
-        //   // console.log(ctx, obj, options);
-        //   // const result = await strapi.services.article.findOne({
-        //   //   id: options.id,
-        //   // });
-        //   return strapi.services.article.findOne({ id: options.id });
-        // },
-      },
       articlesByUser: {
-        description: {
-          description: "return articles by loginned User",
-          resolverOf: "application::article.article.findOne",
-          resolver: async (obj, options, ctx) => {
-            const result = await strapi.services.article.findOne(options);
-
-            console.log(result);
-
-            return result;
-          },
+        description: "return articles by logined user",
+        resolverOf: "application::article.article.findOne",
+        resolver: async (obj, options, { context }) => {
+          const tmp = await strapi.controllers.article.findByUser(context);
+          return tmp;
         },
       },
     },
--- backend/api/article/controllers/article.js
+++ backend/api/article/controllers/article.js
@@ -1,8 +1,18 @@
-'use strict';
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async findByUser(ctx) {
+    const user = await strapi.plugins["users-permissions"].services.user.fetch({
+      id: ctx.state.user.id,
+    });
+    const entity = user.articles;
+
+    return sanitizeEntity(entity, { model: strapi.models.article });
+  },
+};
